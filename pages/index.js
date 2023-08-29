53d65d5239efbcbf8ec21f6226e7c256e78648af
Lucas Nunes Mendes <lucasnunesmendesstudy@outlook.com>
2023-08-29 13:45:11 -0300

    build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import ChatComponent from './componentes/ChatComponent'
+import ChatComponent from '../_componentes/ChatComponent'
 
 const inter = Inter({ subsets: ['latin'] })
 
