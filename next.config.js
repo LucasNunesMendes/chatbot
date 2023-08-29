75e53ed3d922507ebbd594e63671e34324eff6a5
Lucas Nunes Mendes <lucasnunesmendesstudy@outlook.com>
2023-08-29 13:40:20 -0300

    remove env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    OPENAI_API_KEY: "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",
-  },
+
 };
 
 module.exports = nextConfig;
